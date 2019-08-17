8cdc5fa7f1b3aaf63ed7a1fc3b816b1e7af12aa6
ajaneh <ajh363@nau.edu>
2019-08-17 17:57:58 -0500

    each letter is tested to see if can be written, basic durable test passes

--- tests.py
+++ tests.py
@@ -4,4 +4,12 @@ def AcceptAStringAndWriteIt(string, result):
     #don't worry about file in/out yet
     assert string == result
 
-AcceptAStringAndWriteIt("Testing testing", PencilWrite("Testing testing"))
+def TestDurabillityOnlyWritesLettersWhenSharp(string, result):
+    print result
+    assert string == result
+
+
+#AcceptAStringAndWriteIt("Testing testing", PencilWrite("Testing testing"))
+#TestDurabillityOnlyWritesLettersWhenSharp("test", PencilWrite("test"))
+TestDurabillityOnlyWritesLettersWhenSharp("Tes", PencilWrite("Test"))
+#TestDurabillityOnlyWritesLettersWhenSharp("Test ", PencilWrite("Tests"))
--- wordSearch.py
+++ wordSearch.py
@@ -1,8 +1,31 @@
+
 #o simulate, first coarsely and then more faithfully, an ordinary graphite pencil.
 #  It includes writing and editing text, point degradation, using the eraser, and sharpening the pencil.
-
+durability = 4
 #1) When the pencil is instructed to write a string of text on a sheet of paper,
 # the paper should reflect the text that was written.
 def PencilWrite(textToWrite):
+    paper = ""
     #init just return text
-    return textToWrite
+    for letter in textToWrite:
+        paper += CanLetterBeWritten(letter)
+    return paper
+
+#Lowercase letters should degrade the pencil point by a value of one,
+# and capital letters should degrade the point by two.
+# Hence when a pencil with a point durability of four
+def CanLetterBeWritten(letter):
+    global durability
+    print durability,letter
+    if letter in ["", " ", "\n"]:
+        return letter
+    if letter.islower():
+        cost = 1
+    if letter.isupper():
+        cost = 2
+    if cost <= durability:
+        durability -= cost
+        return letter
+    else:
+        return " "
+
