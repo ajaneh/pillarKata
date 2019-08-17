506b418a0d05301b5b1c85e1bd691f0539086f69
ajaneh <ajh363@nau.edu>
2019-08-17 17:43:29 -0500

    input text is returned as string

---
+++ tests.py
@@ -0,0 +1,7 @@
+from pytest import *
+from wordSearch import *
+def AcceptAStringAndWriteIt(string, result):
+    #don't worry about file in/out yet
+    assert string == result
+
+AcceptAStringAndWriteIt("Testing testing", PencilWrite("Testing testing"))
---
+++ wordSearch.py
@@ -0,0 +1,8 @@
+#o simulate, first coarsely and then more faithfully, an ordinary graphite pencil.
+#  It includes writing and editing text, point degradation, using the eraser, and sharpening the pencil.
+
+#1) When the pencil is instructed to write a string of text on a sheet of paper,
+# the paper should reflect the text that was written.
+def PencilWrite(textToWrite):
+    #init just return text
+    return textToWrite
