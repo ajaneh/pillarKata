e71115aa5cc57721b5581155829fc8f87ba3c57b
ajaneh <ajh363@nau.edu>
2019-08-18 11:41:57 -0500

    eraser degradation test passes

--- tests.py
+++ tests.py
@@ -21,15 +21,24 @@ def TestDurabilityResetsWhenSharpened(expectedDurability):
     assert Papers.durability == expectedDurability
 
 def TestEraseFunctionality(initialString, expectedString):
-    Papers = PaperWriter(100, 1, 1)
+    Papers = PaperWriter(100, 1, 4)
     Papers.PencilWrite("a test is a test when tested")
     Papers.erase("test")
     assert initialString != Papers.paper
     assert expectedString == Papers.paper
+
+def TestEraserDegrades():
+    Papers = PaperWriter(100, 1, 5)
+    Papers.PencilWrite("test my test")
+    Papers.erase("test")
+    assert Papers.eraserDurability == 1
+    Papers.erase("my")
+    assert Papers.eraserDurability == 0
 
 test_AcceptAStringAndWriteIt("Testing testing")
 TestDurabilityOnlyWritesLettersWhenSharp("test", "test", 4)
 TestDurabilityOnlyWritesLettersWhenSharp("Tes ", "Test", 4)
 TestDurabilityOnlyWritesLettersWhenSharp("Test ", "Tests", 5)
 TestDurabilityResetsWhenSharpened(4)
-TestEraseFunctionality("a test is a test when tested", "a test is a when tested")
+TestEraseFunctionality("a test is a test when tested", "a test is a      when tested")
+TestEraserDegrades()
